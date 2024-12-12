0dde03b78e70ce3c571e21f91b647a3b2f170f93
archarian <dan15howard@gmail.com>
2024-12-12 10:53:09 +0100

    added sorting of only alphabetic characters and report finalisation, added .gitignore file.

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ def main():
         # Split the text into words once
         words = file_contents.split()
         
+        # Print header
+        print("--- Begin report of books/frankenstien.txt ---")
+
         # Count and print the word count
         count_words(words)
         
         # Count and print the character count
         count_characters(words)
 
+        # Sort and print only the alphabetic character counts
+        sort_on(char_count)
+
+        # Print footer
+        print("--- End report ---")
+
 def count_words(words):
     word_count = len(words)
-    print(f"Word count: {word_count}")
+    print(f"{word_count} words found in document")
+
+char_count = {}
 
 def count_characters(words):
-    char_count = {}
     for word in words:
         lword = word.lower()
         for char in lword:
@@ -24,8 +34,22 @@ def count_characters(words):
                 char_count[char] += 1
             else:
                 char_count[char] = 1
-    print("Character count:", char_count)
     return char_count
+
+def sort_on(char_count):
+    counts = [{"letter":key, "number":value} for key, value in char_count.items()]
+    letter_counts = []
+    for count in counts:
+        if count["letter"].isalpha():
+            letter_counts.append(count)
+
+    sorted_letter_counts = sorted(letter_counts, key=lambda x: x["number"], reverse=True)
+    for sorted_letter_count in sorted_letter_counts:
+        let = sorted_letter_count["letter"]
+        num = sorted_letter_count["number"]
+        print(f"The '{let}' character was found {num} times")
+    return(sorted_letter_counts)
+
 
 main()
 
